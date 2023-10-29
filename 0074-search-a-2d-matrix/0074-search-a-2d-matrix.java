1e0648420ac5b38d6359e37d76b5fe13db27fe0f
susmithanunna <126801279+susmithanunna@users.noreply.github.com>
2023-10-29 13:29:37 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (72.00%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i=0;
+        int j=matrix[0].length-1;
+        int n=matrix.length;
+        while(i<n && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
