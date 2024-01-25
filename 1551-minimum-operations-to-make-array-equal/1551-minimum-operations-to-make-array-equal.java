2b1a9f2f45170291c6be8433b1c15f170da432c3
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-01-25 19:59:30 +0530

    Time: 1 ms (55.09%), Space: 41 MB (20.00%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -2,25 +2,25 @@ class Solution {
     public int minOperations(int n) {
         int mid=-1;
         int ans=0;
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=2*i+1;
-        }
+       // int arr[]=new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i]=2*i+1;
+        // }
         if(n%2==0){
             mid=n/2-1;
-            //int value=(2*mid)+1;
+            int value=(2*mid)+1;
             for(int i=0;i<n;i++){
-            //int t=(2*i)+1;
-            ans=ans+(int)Math.abs(arr[i]-arr[mid]);
+            int t=(2*i)+1;
+            ans=ans+(int)Math.abs(t-value);
            }
             ans=ans/2;
         }
         else{
             mid=n/2;
-            //int value=(2*mid)+1;
+             int value=(2*mid)+1;
             for(int i=mid+1;i<n;i++){
-            //int t=(2*i)+1;
-            ans=ans+(int)Math.abs(arr[i]-arr[mid]);
+            int t=(2*i)+1;
+            ans=ans+(int)Math.abs(t-value);
            }
         }
         
