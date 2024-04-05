1ab32f99e6f984fc5549753ea1b07c3b773d94c5
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-04-05 10:34:57 +0530

    Time: 1 ms (86.47%), Space: 40.4 MB (88.50%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,26 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-//         // if(x==0){
-//         //     return 0;
-//         // }
-//         long l=1;
-//         long r=x;
+        if(x==0 || x==1){
+            return x;
+        }
+        long l=1;
+        long r=x;
         
-//         while(l<=r){
-//           int mid=(int)(l+r)/2;
-//             int sq=(int)(mid*mid);
-//             if(sq==x){
-//                 return mid;
-//             }
-//             else if(sq<x){
-//                 l=(long)mid+1;
-//             }
-//             else{
-//                 r=(long)mid-1;
-//             }
+        while(l<=r){
+          long mid=(l+r)/2;
+            long sq=(mid*mid);
+            if(sq==x){
+                return (int)mid;
+            }
+            else if(sq<x){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
         
-//     }
-//         return (int)r;
-        return (int)Math.sqrt(x);
+    }
+        return (int)r;
+      //  return (int)Math.sqrt(x);
 }
 }
