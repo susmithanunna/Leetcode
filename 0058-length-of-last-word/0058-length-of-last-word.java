bed2af518c45ad79c3ac37c2844e5962032250b6
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-04-19 11:58:26 +0530

    Time: 5 ms (11.27%), Space: 42.2 MB (8.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,13 +3,12 @@ class Solution {
         //ArrayList<String> a1=new ArrayList<>();
         String s1="";
         s=s.trim();
-        for(int i=0;i<s.length();i++){
+        for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)!=' '){
                 s1=s1+s.charAt(i);
             }
             else{
-                //a1.add(s1);
-                s1="";
+               break;
             }
         }
         return s1.length();
