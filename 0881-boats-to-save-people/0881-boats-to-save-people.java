081e29f6399a81774487328af2b54cda03e22a58
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-05-04 13:03:23 +0530

    Time: 16 ms (97.17%), Space: 55 MB (17.06%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boats=0;
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+            }
+            j--;
+            boats++;
+        }
+        
+        return boats;
+    }
+}
