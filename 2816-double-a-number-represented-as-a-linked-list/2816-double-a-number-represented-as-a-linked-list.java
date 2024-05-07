3e57dffdff5a888ac2e145c0e17f899f09f7743a
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-05-07 21:54:20 +0530

    Time: 4 ms (77.89%), Space: 46.9 MB (26.46%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -10,19 +10,45 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        int carry=multiply(head);
-        if(head!=null && carry!=0){
-            head=new ListNode(carry,head);
+        // int carry=multiply(head);
+        // if(head!=null && carry!=0){
+        //     head=new ListNode(carry,head);
+        // }
+        // return head;
+        head=reverse(head);
+        int carry=0;
+        ListNode temp=head;
+        while(temp!=null){
+            int value=temp.val*2+carry;
+            temp.val=value%10;
+            carry=value/10;
+           if(temp.next==null && carry!=0){
+               temp.next=new ListNode(carry);
+               temp=temp.next;
+           }
+            temp=temp.next;
         }
-        return head;
+        return reverse(head);
         
     }
-    public int multiply(ListNode head){
-        if(head==null){
-            return 0;
+    // public int multiply(ListNode head){
+    //     if(head==null){
+    //         return 0;
+    //     }
+    //     int value=head.val*2+(multiply(head.next));
+    //     head.val=value%10;
+    //     return value/10;
+    // }
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode nex=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nex;
         }
-        int value=head.val*2+(multiply(head.next));
-        head.val=value%10;
-        return value/10;
+        return prev;
+        
     }
 }
