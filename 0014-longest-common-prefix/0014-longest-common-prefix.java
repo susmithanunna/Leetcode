1fa36a1d6a31ad97632690706eb71143edfe4980
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-05-27 16:13:15 +0530

    Time: 1 ms (75.46%), Space: 41.3 MB (59.45%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -8,18 +8,12 @@ class Solution {
            else if(ans.length()<strs[i].length()){
                strs[i]=strs[i].substring(0,ans.length());
            }
-           //System.out.println("strs[i] "+strs[i]);
-           //System.out.println("str.lnegth "+strs[i].length() +" anslength "+ans.length());
            for(int j=0;j<strs[i].length();j++){
-               //System.out.println("ans :"+ans +" strs[i] "+strs[i]+ "j: "+j);
-               //System.out.println("ans char "+ans.charAt(j)+"  str char "+strs[i].charAt(j));
                if(ans.charAt(j)!=strs[i].charAt(j)){
                    ans=ans.substring(0,j);
                    break;
                }
            }
-          // System.out.println(ans);
-           
            if(ans.length()==0){
                return "";
            }
